07504a35332a3dbb1e445d3fc3f6b23f59716152
barry_Cin <barry@cinnamon.is>
2019-10-09 09:42:15 +0700

    Add pretrain func

--- train.py
+++ train.py
@@ -4,16 +4,87 @@ from torchsummary import summary
 from torch.utils.data import DataLoader
 from torchvision.datasets.mnist import MNIST
 from torchvision import transforms
+import itertools
+from sklearn.mixture import GaussianMixture
 
 from model.vae import *
 from model.dec import *
 from utils.utils import *
 import matplotlib.pyplot as plt
 
+def pretrain(model: ClusteringBasedVAE, train_dataloader, val_dataloader, **params):
+    num_pretrained_epoch = params.get('pretrained_epochs', 10)
+    save_path = params.get('save_path', 'output/model')
+    dataset_name = params.get('dataset_name', '')
+    device = torch.device('cuda' if (torch.cuda.is_available()) else 'cpu')
+    model = model.to(device)
+
+    res_loss = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(itertools.chain(model.vae.encoder.parameters(),
+                                model.vae.decoder.parameters()))
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    print('Pretrains VAE using only reconstruction loss...')
+    for pre_epoch in range(num_pretrained_epoch):
+        total_loss = 0.0
+        iters = 0
+        for i, data in enumerate(train_dataloader):
+            x = data[0]
+
+            # Flatten 28x28 to 1x784 on mnist dataset
+            if dataset_name == 'mnist':
+                x = x.view(x.size()[0], -1)
+            
+            x = x.to(device)
+            # Forward pass
+            x_decoded, _, _, _ = model.vae(x)
+            loss = res_loss(x_decoded, x)
+            total_loss = loss.detach().cpu().numpy()
+
+            # Calculate gradient and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            iters += 1
+        
+        print('VAE resconstruction loss: ', total_loss / (len(train_dataloader) / iters))
+    
+    model.vae.encoder.sampling.log_var.load_state_dict(model.vae.encoder.sampling.mu.state_dict())
+
+    Z = []
+    Y = []
+    with torch.no_grad():
+        for x, y in train_dataloader:
+            x = x.to(device)
+            mu, log_var = model.vae.encoder(x)
+            assert F.mse_loss(mu, log_var) == 0
+            Z.append(mu)
+            Y.append(y)
+
+    Z = torch.cat(Z, 0).detach().cpu().numpy()
+    Y = torch.cat(y, 0).detach().numpy()
+    gmm = GaussianMixture(n_components=model.cluster.n_centroids, covariance_type='diag')
+    predict = gmm.fit_predict(Z)
+
+    print('Accuracy = {:.4f}%'.format(cluster_accuracy(predict, Y)[0] * 100))
+
+    model.cluster.mu_param = torch.from_numpy(gmm.means_).to(device).float()
+    model.cluster.lambda_param = torch.log(torch.from_numpy(gmm.covariances_).to(device).float())
+    model.cluster.theta_param = torch.from_numpy(gmm.weights_).to(device).float()
+
+    torch.save(model.state_dict(), 'results/model/pretrained/pretrained_model.pk')
+
+
+
+
 def train(model, train_dataloader, val_dataloader, **params):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0002, eps=1e-4)
     # optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     num_epochs = params.get('epochs', 10)
+    num_pretrained_epoch = params.get('pretrained_epochs', 10)
     save_path = params.get('save_path', 'output/model')
     dataset_name = params.get('dataset_name', '')
 
