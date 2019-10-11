93e724ae5f9e6a77539ff6b5746ffbef3211480c
baohq1595 <hqb1595@gmail.com>
2019-10-11 22:59:40 +0700

    Modify log_gausian pdf func

--- utils/distributions.py
+++ utils/distributions.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -23,8 +23,9 @@ def log_gaussian(x, mu, log_var):
     :param log_var: log variance of distribution
     :return: log N(x|µ,σ)
     """
-    log_pdf = -0.5 * math.log(2 * math.pi) - log_var / 2 - (x - mu)**2 / (2 * torch.exp(log_var))
-    return torch.sum(log_pdf, dim=-1)
+    # log_pdf = -0.5 * math.log(2 * math.pi) - log_var / 2 - (x - mu)**2 / (2 * torch.exp(log_var))
+    # return torch.sum(log_pdf, dim=-1)
+    return -0.5 * (torch.sum(np.log(np.pi * 2) + log_var + (x - mu) ** 2 / torch.exp(log_var), 1))
 
 
 def log_standard_categorical(p):
